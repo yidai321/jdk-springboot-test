6f9fd49a1512617aef15d39f9ce11bd8809512b3
OmO <2037164933@qq.com>
2024-07-15 00:22:28 +0800

    init

---
+++ src/test/java/org/lxg/JdkSpringbootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lxg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdkSpringbootTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
